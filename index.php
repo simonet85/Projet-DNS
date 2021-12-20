d0841062dde15b8f938e20e054a26be4e68bd071
bakary2025 <dbakary57@gmail.com>
2021-12-20 18:47:11 +0100

    initial commit

--- index.php
+++ index.php
@@ -5,25 +5,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css" />
     <title>Accueil</title>
 </head>
 <body>
 <?php 
     require "connexion.php" ;
+
     // If form submitted, insert values into the database.
     if (isset($_POST['submit'])){
-        $nom = stripslashes($_POST['nom']);
-        $nom = mysqli_real_escape_string($con,$nom);
-        $prenom = stripslashes($_POST['prenom']);
-        $prenom = mysqli_real_escape_string($con,$prenom);
-        $email = stripslashes($_POST['email']);
-        $email = mysqli_real_escape_string($con,$email);
-        $password = stripslashes($_POST['motdepasse']);
-        $password = mysqli_real_escape_string($con,$password);
+        echo $nom = stripslashes($_POST['nom']);
+        echo $nom = mysqli_real_escape_string($con,$nom);
+        echo $prenom = stripslashes($_POST['prenom']);
+        echo $prenom = mysqli_real_escape_string($con,$prenom);
+        echo $email = stripslashes($_POST['email']);
+        echo $email = mysqli_real_escape_string($con,$email);
+        echo $password = stripslashes($_POST['motdepasse']);
+        echo $password = mysqli_real_escape_string($con,$password);
       
         $query = "INSERT into `users` (nom, prenom, email, password)
     VALUES ('$nom', '$prenom', '$email', '".md5($password)."')";
             $result = mysqli_query($con,$query);
+
+            echo $result;
 
             if($result){
                 echo "<div class='form'>
@@ -32,14 +36,18 @@
             }
         }else{
 ?>
+<div class="form">
+<h1>Registration</h1>
     <form method="post">
-        Nom: <input type="text" name="nom"><br>
-        Prenom: <input type="text" name="prenom"><br>
-        Email: <input type="email" name="email"><br>
-        Mot de passe: <input type="password" name="motdepasse"><br>
+        <input type="text" name="nom" placeholder="Nom"><br>
+        <input type="text" name="prenom" placeholder="Prenom"><br>
+        <input type="email" name="email" placeholder="Email"><br>
+        <input type="password" name="motdepasse" placeholder="Mot de passe"><br>
         <input type="submit" name="submit" value="Enregistrer">
     </form>
-
+    <p>Cliquer ici pour vous <a href='login.php'>Connecter</a></p>
+ 
+</div>
 <?php } ?>
 
 </body>
