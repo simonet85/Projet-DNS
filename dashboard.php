4d768fc4e4b103af73cca0372e3ed581f8e55af0
simonet85 <christian.85@live.fr>
2021-12-22 13:10:45 +0100

    Add css

--- dashboard.php
+++ dashboard.php
@@ -12,14 +12,39 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
-    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="./css/style.css" />
 </head>
 <body>
-<div class="form">
-    <p>Welcome <?php echo $_SESSION['email']; ?>!</p>
-    <p>This is secure area.</p>
-    <p><a href="dashboard.php">Dashboard</a></p>
-    <a href="logout.php">Logout</a>
+<div class="nav">
+    <div class="nav-left">
+        
+        <ul class="nav__ul-left">
+            <li class="nav__li-left">
+                <a class="nav__link-left" href="dashboard.php">Tableau de bord</a>
+            </li>
+        </ul>
+
+    </div>
+    <div class="nav-right">
+        <ul class="nav__ul-right">
+            <li class="nav__li-right">
+                <a class="nav__link-right" href="dashboard.php">
+                    Bienvenue : <?php echo $_SESSION['email']; ?>
+                </a>
+            </li>
+            <li class="nav__li-right">
+                <a class="nav__link-right" href="logout.php">
+                  Déconnexion
+                </a>
+            </li>
+        </ul>
+    </div>
+
 </div>
+<div class="container">
+    <p class="dash__title">Zone Administrative</p>
+</div>
+
+  
 </body>
 </html>
